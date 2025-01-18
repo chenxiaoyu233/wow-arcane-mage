b8b8190fc71f7521fe3c899675b2031fca9308b8
chenxiaoyu233 <chenxiaoyu233@smail.nju.edu.cn>
2025-01-19 00:28:04 +0800

    [upd] add windows support for the update script

--- update.sh
+++ update.sh
@@ -1,38 +1,57 @@
 #!/bin/bash
 
-echo "Read the file list from remote"
+echo "fetching file lists"
 
-filename=$( 
-    curl "http://downloads.simulationcraft.org/nightly/?C=M;O=D" \
-    | grep -Eo 'href="simc[^"]*\.dmg"' \
-    | head -n 1 \
-    | sed 's/href="\(.*\)"/\1/'
-)
+# platform = win
+if [[ $# -ne 0 && $1 = "win" ]]; then 
+    filename=$( 
+        curl "http://downloads.simulationcraft.org/nightly/?C=M;O=D" \
+        | grep -Eo 'href="simc[^"]*win64\.7z"' \
+        | head -n 1 \
+        | sed 's/href="\(.*\)"/\1/'
+    )
+# platform = mac
+else 
+    filename=$( 
+        curl "http://downloads.simulationcraft.org/nightly/?C=M;O=D" \
+        | grep -Eo 'href="simc[^"]*\.dmg"' \
+        | head -n 1 \
+        | sed 's/href="\(.*\)"/\1/'
+    )
+fi
 
-echo "Update SIMC if needed"
+echo "downloading $filename"
 
-curl -z simc.dmg "http://downloads.simulationcraft.org/nightly/$filename" --output simc.dmg
+curl -z simc "http://downloads.simulationcraft.org/nightly/$filename" --output simc
 
 echo "Extracting files"
 
-# Path to the .dmg file
-dmg_file="./simc.dmg"
+function extractDMG {
+    # Path to the .dmg file
+    dmg_file="./simc"
 
-# Mount the DMG file
-hdiutil attach "$dmg_file"
+    # Mount the DMG file
+    hdiutil attach "$dmg_file"
 
-# Wait a moment to ensure it's mounted
-sleep 2
+    # Wait a moment to ensure it's mounted
+    sleep 2
 
-# Get the name of the mounted volume (assuming it's the first one)
-mounted_volume=$(ls /Volumes | grep "Simulationcraft")
+    # Get the name of the mounted volume (assuming it's the first one)
+    mounted_volume=$(ls /Volumes | grep "Simulationcraft")
 
-# Copy the contents to the destination folder
-destination_folder="./SIMC"
-#cp -R "/Volumes/$mounted_volume/" "$destination_folder"
-rsync -r --progress "/Volumes/$volume_name/" "$destination_folder"
+    # Copy the contents to the destination folder
+    destination_folder="./SIMC"
+    #cp -R "/Volumes/$mounted_volume/" "$destination_folder"
+    rsync -r --progress "/Volumes/$volume_name/" "$destination_folder"
 
-# Unmount the DMG file
-hdiutil detach "/Volumes/$mounted_volume"
+    # Unmount the DMG file
+    hdiutil detach "/Volumes/$mounted_volume"
 
-echo "DMG file extracted to $destination_folder"
+    echo "DMG file extracted to $destination_folder"
+}
+
+if [[ $# -ne 0 ]]; then
+    7z e ./simc -o ./SIMC
+else
+    extractDMG
+fi
